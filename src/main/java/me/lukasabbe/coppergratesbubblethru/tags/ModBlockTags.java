861e405ac75a5c76b4df9c92de0ab9849343588c
lukas <67807954+lukasabbe@users.noreply.github.com>
2026-06-16 17:58:59 +0200

    26.2

--- src/main/java/me/lukasabbe/coppergratesbubblethru/tags/ModBlockTags.java
+++ src/main/java/me/lukasabbe/coppergratesbubblethru/tags/ModBlockTags.java
@@ -1,16 +1,11 @@
 package me.lukasabbe.coppergratesbubblethru.tags;
 
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class ModBlockTags {
-    private final static List<Block> copperGrates = Arrays.asList(Blocks.COPPER_GRATE, Blocks.EXPOSED_COPPER_GRATE, Blocks.WAXED_COPPER_GRATE, Blocks.OXIDIZED_COPPER_GRATE, Blocks.WEATHERED_COPPER_GRATE, Blocks.WAXED_EXPOSED_COPPER_GRATE, Blocks.WAXED_OXIDIZED_COPPER_GRATE, Blocks.WAXED_WEATHERED_COPPER_GRATE);
     public static boolean isAWaterLoggedCopperGrates(BlockState state){
-        return copperGrates.stream().anyMatch(t -> t.defaultBlockState().is(state.getBlock())) && state.hasProperty(BlockStateProperties.WATERLOGGED) && state.getValue(BlockStateProperties.WATERLOGGED);
+        return Blocks.COPPER_GRATE.asList().stream().anyMatch(t -> t.defaultBlockState().is(state.getBlock())) && state.hasProperty(BlockStateProperties.WATERLOGGED) && state.getValue(BlockStateProperties.WATERLOGGED);
     }
 }
